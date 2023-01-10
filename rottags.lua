16c32a1e64af122db73e4d179cbbdd47e08b3cac
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-01-10 15:21:25 +0700

    Update rottags.lua

--- rottags.lua
+++ rottags.lua
@@ -4,6 +4,10 @@ else
     Lihatin = "Gak"
 end
 
+if Jastip then
+	Gajian = true
+end
+
 LogoPartai = {
     "https://raw.githubusercontent.com/ditampol-tzuyoon/jnck/main/partai/demokrat.png",
     "https://raw.githubusercontent.com/ditampol-tzuyoon/jnck/main/partai/gerindra.png",
@@ -262,6 +266,104 @@ function odnotice(logger)
     pipe:close()
 end
 
+function odplant(logger)
+
+    MenitRdp = (os.date("*t", os.time()).min) + 0
+    JamRdp = (os.date("*t", os.time()).hour) + Selisih
+
+    if MenitRdp < 10 then
+        myMenit = "0"..MenitRdp
+    else
+        myMenit = MenitRdp
+    end
+
+    if JamRdp >= 24 then
+        JamRdp = JamRdp - 24
+    end
+
+    if JamRdp < 10 then
+        myJam = "0"..JamRdp
+    else
+        myJam = JamRdp
+    end
+
+    if block == 4584 then
+        Thumbs = "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/pepper.webp"
+    elseif block == 5666 then
+        Thumbs = "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/LaserGrid.webp"
+    elseif block == 3004 then
+        Thumbs = "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/ftank.webp"
+    elseif block == 340 then
+        Thumbs = "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/chand.webp"
+    elseif block == 8640 then
+        Thumbs = "https://raw.githubusercontent.com/ditampol-tzuyoon/Growtopia/main/sambalado.webp"
+    else
+        Thumbs = ""
+    end
+
+	TanamWorld = "Planting ("..nPlant.." of "..#WorldPlant.." Worlds)"
+    Warna = 7405312
+    Banteng = math.random(1, #LogoPartai)
+
+    SendPlant = PlantHook.."/messages/"..PlantMsg
+
+    local script = [[
+        $w = "]]..SendPlant..[["
+
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+        [System.Collections.ArrayList]$embedArray = @()
+        $descriptions = ']].. logger ..[['
+        $color       = ']]..Warna..[['
+
+        $footerObject = [PSCustomObject]@{
+            text = 'Bonus Planting' + "`n" + '(Time : ]]..myJam..[[:]]..myMenit..[[)'
+            icon_url = ']].. Thumbs ..[['
+        }
+
+        $thumbnailObject = [PSCustomObject]@{
+            url = ']].. Thumbs ..[['
+        }
+
+        $authorObject = [PSCustomObject]@{
+            name = "Continue Plant || Author : Ohdear#2320"
+            url = "https://discord.gg/TjVwdgma74"
+            icon_url = "]]..LogoPartai[Banteng]..[["
+        }
+
+        $fieldArray = @(
+
+            @{
+                name = "]]..TanamWorld..[["
+                value = "]]..IngfoPlant()..[["
+                inline = "false"
+            }
+        )
+
+        $embedObject = [PSCustomObject]@{
+            description = $descriptions
+            color       = $color
+            footer      = $footerObject
+            thumbnail   = $thumbnailObject
+            author      = $authorObject
+            fields      = $fieldArray
+        }
+
+        $embedArray.Add($embedObject) | Out-Null
+
+        $Body = [PSCustomObject]@{
+            embeds = $embedArray
+            'username' = ']]..getBot().name..[[ | OD2320'
+        }
+
+        Invoke-RestMethod -Uri $w -Body ($Body | ConvertTo-Json -Depth 4) -Method Patch -ContentType 'application/json'
+    ]]
+
+    local pipe = io.popen("powershell -command -", "w")
+    pipe:write(script)
+    pipe:close()
+end
+
 function ohdsay(logger, hookURL, Pings)
 
 if namapack == "crackers" then
@@ -365,13 +467,15 @@ AllDelay = "\n~\nBreak / Place / HT / Plant / World : **("
                 Muncul = true
             end
         end
-        if WorldPack:upper() == WorldSkrg then
-            Muncul = true
+        for _, v in pairs(WorldPack) do
+            if v:upper() == WorldSkrg then
+                Muncul = true
+            end
         end
         if WorldAxe:upper() == WorldSkrg then
             Muncul = true
         end
-        if Gajian then
+        if Jastip then
             if WorldGaji:upper() == WorldSkrg then
                 Muncul = true
             end
@@ -429,10 +533,6 @@ AllDelay = "\n~\nBreak / Place / HT / Plant / World : **("
         $thumbnailObject = [PSCustomObject]@{
             url = ']].. Thumbs ..[['
         }
-		
-	$imageObject = [PSCustomObject]@{
-        	url = ']].. imagez ..[['
-    	}
 
         $authorObject = [PSCustomObject]@{
             name = "]]..Judulz..[[ || Author : Ohdear#2320"
@@ -510,7 +610,7 @@ AllDelay = "\n~\nBreak / Place / HT / Plant / World : **("
             thumbnail   = $thumbnailObject
             author      = $authorObject
             fields      = $fieldArray
-		image       = $imageObject
+		    image       = $imageObject
         }
 
         $embedArray.Add($embedObject) | Out-Null
